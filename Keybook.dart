c174631b0a47fac88a87c6674844ebc9564254c0
RAMEEZ KAYANI <101308522+rameezkayani1@users.noreply.github.com>
2022-03-13 06:25:22 -0700

    Add files via upload

---
+++ Keybook.dart
@@ -0,0 +1,134 @@
+import 'dart:developer';
+
+import 'dart:math';
+
+main() {
+  /* double kilometer = 1.609;
+  double miles = 25;
+  double distance = kilometer * miles;
+  print("total Miles is $distance");
+  */
+  // Question nNo.5
+  final ali = 25;
+  final saqib = 36;
+  final raza = 35;
+  final usman = 17;
+  final totalscore = ali + saqib + raza + usman;
+  print("The sum of all Player Score is $totalscore");
+  final theproduct = ali * saqib * raza * usman;
+  print("The product of all Player $theproduct");
+  final average = totalscore / 4;
+  print("The average Score of all player is $average");
+  // Qestion no 6
+  final age = 23;
+  final month = 23 * 12;
+  print("My age in Month is $month");
+  final days = age * 365;
+  print("My age in Days $days");
+  // Question 7
+  final square = 25;
+  final Tsquare = square * square;
+  print("The Square re of $square is $Tsquare ");
+
+  // Question no 8
+
+  // Question no 9
+  final petrol = 12;
+  final distance = 52;
+  final totalDistance = petrol * distance;
+  print(
+      "I have ${petrol}L Gas and i covered & travled distance is ${totalDistance}KM. ");
+
+  //  Question no 10
+  final totalSudent = 25;
+  final Sfees = 50;
+  final collectedFess = totalSudent * Sfees;
+  print(
+      "Total Student of Class is $totalSudent and per student fee is $Sfees  . The collected fee is $collectedFess");
+
+  // Question no 11
+
+  final ftemeperture = 25;
+  final Celsius = 5 / 9 * (ftemeperture - 32);
+  print("The termperature in Celsius is $Celsius");
+
+  //  Question no 12
+
+  final seriesNo = 123;
+  final a = seriesNo / 100;
+  final b = seriesNo % 100;
+  final c = seriesNo / 10;
+  final d = seriesNo % 10;
+  print("The serle NO is $a\n . $b/n $c /n $d ");
+
+  // Question no 13
+  print("1  2  3  4  5\n  6  7  8  9  10");
+
+  // Quetion no 14
+  final Length = 25;
+  final width = 25;
+  final height = 42;
+  final volume = Length * width * height;
+  print("The Volume of Cube is $volume");
+
+  // Quetion no 15
+  final x2 = 25;
+  final x1 = 25;
+  final x3 = x2 - x1;
+  final x4 = x3 * x3;
+  final y2 = 35;
+  final y1 = 25;
+  final y3 = y2 - y1;
+  final y4 = y3 * y3;
+  final distancePoint = (x4 + y4) / .5;
+  print("The of distnce of two Point is $distancePoint");
+
+  // Qestion no 16
+  // Qustion no 17
+
+  final radius = 25;
+  final angle = 14;
+  final angleLength = radius * angle;
+  print("Length =$angleLength");
+
+// Qustion no 18
+  final pounds = 48;
+  final kiloGram = pounds / 2.205;
+  print("$pounds Poundas is equla to $kiloGram Kiologram");
+
+  // Question no 19
+
+  final radians = 14;
+  final thota = 85;
+  final areaSector = (radians * radians * thota) / 2;
+  print("The Area of Sector is $areaSector");
+
+  //  Quesiton no 20
+
+  /*
+  final logritumess = 20;
+ final anssdf = log(logritumess)/log(2);
+*/
+
+// Question no 21
+
+  final charac = d;
+  print((charac + 1));
+
+  // Question no 22
+
+  // Question no 23
+  final basicSalary = 18000;
+  final GrossSalary = basicSalary + (basicSalary * 0.35) + (basicSalary * 0.25);
+  print("The Gross Salary is $GrossSalary");
+
+  // Question no 24
+
+  // Question no 25
+  /*
+  final QuestionP = 21;
+  final QuestionR = 10;
+  final QuestionT = 14;
+  final QuestionAM = QuestionP * pow(1 + QuestionR / 100);
+*/
+}
